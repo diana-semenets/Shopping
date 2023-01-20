94c139fd88be05bd291ef03483002be5847c63dc
diana-semenets <110393311+diana-semenets@users.noreply.github.com>
2023-01-20 23:25:35 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function showDate () {
+    let now = new Date();
+  
+    let span = document.querySelector(".news__cards-data");
+  
+        let date = now.getDate();  
+        let year = now.getYears();              
+        let months = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+        let month = months[now.getMonth()];
+     
+    span.innerHTML = `${date}.${month}.${year} `;     
+}
+showDate()
