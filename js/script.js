2eaae9129bc1db452d19419429832af1711e402b
diana-semenets <110393311+diana-semenets@users.noreply.github.com>
2023-01-24 23:29:44 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
+
+
 function showDate () {
     let now = new Date();
   
     let span = document.querySelector(".news__cards-data");
-  
-        let date = now.getDate();  
-                 
-        let months = ["01","02","03","04","05","06","07","08","09","10","11","12"];
-        let month = months[now.getMonth()];
+    let date = now.getDate();               
+    let months = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+    let month = months[now.getMonth()];
      
     span.innerHTML = `${date}.${month}`;     
 }
 
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
 
 
 
