2d7f7adb0f205170db4ddff586eda7cd6c08be7d
diana-semenets <110393311+diana-semenets@users.noreply.github.com>
2023-01-24 20:54:23 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ function showDate () {
     let span = document.querySelector(".news__cards-data");
   
         let date = now.getDate();  
-        let year = now.getYear();              
+                 
         let months = ["01","02","03","04","05","06","07","08","09","10","11","12"];
         let month = months[now.getMonth()];
      
-    span.innerHTML = `${date}.${month}.${year} `;     
+    span.innerHTML = `${date}.${month}`;     
 }
 
 
