fa70361c1161513359f32b711b0a032784986d30
diana-semenets <110393311+diana-semenets@users.noreply.github.com>
2023-02-13 22:17:33 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,17 +8,26 @@ let menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
+    //document.body.style.overflow = 'hidden';
+
+   if (menuBtn.classList.contains('active')) {
     document.body.style.overflow = 'hidden';
-    
+   }else {
+    document.body.style.overflow = '';
+   }
 })
 menu.addEventListener('click', (e) => {
-    if (e.target === menu || e.target === menuBtn) {
+    if (e.target === menu) {
         menu.classList.remove('active');
         menuBtn.classList.remove('active');  
         document.body.style.overflow = '';   
            
     }
 });
+
+
+
+
 
 //---------------------//
 
